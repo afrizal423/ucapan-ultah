58f7be40aa8b77100af0b5f5fe81bd719ed84e6d
afrizal423 <afrizal2499@gmail.com>
2022-06-07 11:31:22 +0700

    fix favicon

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  transpileDependencies: true,
+  pwa: {
+    name: 'Happy Birthday!!!',
+    iconPaths: {
+       favicon32: 'img/favicon.png',
+       faviconSVG: 'img/favicon.png',
+       favicon16: 'img/favicon.png',
+       appleTouchIcon: 'img/favicon.png',
+       maskIcon: 'img/favicon.png',
+       msTileImage: 'img/favicon.png'
+    }
+  }
+}
